c46a43f3c08b4b01a70a3bbb68b699c49837efd6
Nathaniel Tucker <nmtucker17@gmail.com>
2023-02-12 22:26:45 -0700

    Merge remote and local

--- Project_Ignition/client/context/authentication.ts
+++
@@ -1,6 +0,0 @@
-import { type Auth } from 'firebase/auth';
-import React from 'react';
-import { auth } from '../firebase/firebase.config';
-export { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
-
-export const FirebaseContext = React.createContext<Auth>(auth);
